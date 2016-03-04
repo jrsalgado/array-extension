9532a00cffb6ee8bb60a35ca527bffb3695434c2
Jayro Salgado <jsalgado@nearsoft.com>
2016-03-04 16:22:02 -0700

    callback should be called with value and index as parmeters

--- cool-stuff/each.js
+++ cool-stuff/each.js
@@ -2,11 +2,13 @@ module.exports = coolEach;
 function coolEach(callback){
   var i=0,
   length = this.length;
+  
   if(!callback || !(callback instanceof Function)){
     throw new Error('Array.each method needs callback function');
   }
+  
   for(; i < length; i++){
-      callback();
+      callback(this[i], i, this);
   }
   return this
 }
--- test/specEach.js
+++ test/specEach.js
@@ -40,4 +40,41 @@ describe('cool Each', function () {
     done();
   });
   
+  it('should call callback with value and index as params', function(done){
+    var arr = [1,2], cb = sinon.spy();
+    arr.each(cb);
+
+    cb.calledTwice.should.be.equals.true;
+    cb.firstCall.calledWith(1, 0, arr).should.be.true;
+    cb.secondCall.calledWith(2, 1, arr).should.be.true;
+
+    done();
+  });
+  
+  it('should console the magic!!!', function(done){
+    var txts, people, expectedTxts;
+      
+    txts = [];
+    
+    people = [
+      {name: 'king slayer', age: 40 },
+      {name: 'king jeofrey', age: 15 },
+      {name: 'jon snow', age: 23 },
+      {name: 'sansa', age: 16 },
+      {name: 'little finger', age: 45}];
+      
+   expectedTxts = [ 
+     '1.­ king slayer is 40 years old',
+     '2.­ king jeofrey is 15 years old',
+     '3.­ jon snow is 23 years old',
+     '4.­ sansa is 16 years old',
+     '5.­ little finger is 45 years old' ];
+  
+      people.each(function(x, i){
+        txts.push((i + 1) + '.­ ' + x.name + ' is ' + x.age + ' years old');
+      });
+      txts.should.be.deep.equal(expectedTxts);
+      done();
+  });
+  
 });
