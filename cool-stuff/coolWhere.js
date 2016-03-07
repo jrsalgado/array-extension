2f0acdd6ad528ecf8ceda88430f36319fce3f6b0
Jayro Salgado <jsalgado@nearsoft.com>
2016-03-07 14:18:03 -0700

    hire people with PHP skills assert

--- cool-stuff/coolWhere.js
+++ cool-stuff/coolWhere.js
@@ -3,11 +3,11 @@ module.exports = Array.prototype.where = coolWhere;
 
 function coolWhere(callback){
   var el,i=0, length = this.length, match=[];
-  
+
   for (; i < length; i++) {
     el = this[i];
     (callback(el) === true)? match.push(el) : null;
   }
-  
+
   return match;
 }
--- test/specWhere.js
+++ test/specWhere.js
@@ -5,12 +5,36 @@ sinon = require('sinon'),
 should = chai.should(),
 expect = chai.expect;
 
-describe('cool Each', function () {
+describe('cool where', function () {
   
-  it('this should work', function () {
-        var x = [1,2].where(function(el){
-          return el == 1;
-        });
-        console.log(x);
+  it('this return the even numbers', function () {
+    var x = [1,2,3,4,5,6,7,8,9,10].where(function(num){
+          return !(num % 2);
+    });
+    
+    x.should.be.eql([2,4,6,8,10]);
+    
+  });
+  
+  it('should hire people without PHP skills', function () {
+    var people = [
+    {name: 'pedro', age: 29, skills: ['C#', 'Asp.Net', 'OOP'] },
+    {name: 'juan', age: 23, skills: ['PHP', 'Drink tea'] },
+    {name: 'pablo', age: 26, skills: ['RoR', 'HTML/CSS'] }
+    ],
+    hired = [];
+    
+    hired = people.where(function(dev){
+      return dev.skills.where( matchSkill ).length == 0;
+    });
+    
+    hired.should.be.an('array').with.length(2);
+    hired.should.have.deep.property('[0].name', 'pedro');
+    hired.should.have.deep.property('[1].name', 'pablo');
+    
+    function matchSkill(skill) {
+      return skill == 'PHP';
+    }
+    
   });
 });
